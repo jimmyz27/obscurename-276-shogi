270e6d0f1b62067fefc2405b997e212672c12f21
jimmy zhong <jimmyzhong@d142-058-163-047.wireless.sfu.ca>
2016-10-20 17:40:12 -0700

    WIP

---
+++ display.jl
@@ -0,0 +1,51 @@
+#load sudo data into a database
+# replay the moves using shogi.jl
+# using preexisting data base files.
+#use a while to go throught play.jl and display all the filesize
+#pass in arguent for file name
+#call the shogi file for function moves.
+include("shogi.jl")
+include("database.jl")
+
+
+#extract from database first command
+
+function get_name_of_piece()
+
+end
+
+red_pieces = Pieces("red")
+black_pieces = Pieces("black")
+GB = Board()
+
+#length is the total number of moves.
+
+
+function run_from_Start(length,red,black)
+   i = 1
+   fill_red(red)
+   fill_black(black)
+   init_board(GB,red,black)
+   while(i<=length)
+     #get the 2nd argument from the target coordinate
+     #in the right order.
+     #target cords
+     ## if errors its here.
+     #target_cords = get_targetCords(filename,i)
+
+     #print(target_cords)
+
+    #   if(i%2==0)
+    #   piece = red_pieces.activeS[(target_cords)]
+    #  else
+    #    piece = black_pieces.activeS[(target_cords)]
+    #  end
+
+      #move_piece(GB,red_pieces,black_pieces,piece,target_cords)
+     display_board(GB,red_pieces,black_pieces)
+     i = i+1
+     #the time
+   end
+
+end
+run_from_Start(2,red_pieces,black_pieces)
