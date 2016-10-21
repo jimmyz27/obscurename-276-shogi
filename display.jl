f06d0117dc062438cd8fbdff9772c662042cd475
jimmy zhong <jimmyzhong@d142-058-163-047.wireless.sfu.ca>
2016-10-20 19:39:46 -0700

    working test some remaning things

--- display.jl
+++ display.jl
@@ -16,35 +16,41 @@ end
 
 red_pieces = Pieces("red")
 black_pieces = Pieces("black")
-GB = Board()
 
+GB = Board()
+fill_red(red_pieces)
+println()
+fill_black(black_pieces)
+init_board(GB,red_pieces,black_pieces)
 #length is the total number of moves.
 
 
 function run_from_Start(length,red,black)
    i = 1
-   fill_red(red)
-   fill_black(black)
-   init_board(GB,red,black)
+
    while(i<=length)
      #get the 2nd argument from the target coordinate
-
+     #moves the pieces inversed
+     #only moves the red pieces.
      source_cords = get_sourceCords(filename,i)
      target_cords = get_targetCords(filename,i)
-     print("source",source_cords)
-     print("target",target_cords)
+     println("source",source_cords)
+     println("target",target_cords)
+     println("i ",i%2)
+      if i%2==1
+        piece = red_pieces.activeS[(source_cords)]
+        move_piece(GB,red_pieces,black_pieces,piece,target_cords)
+      else
+        piece = black_pieces.activeS[(source_cords)]
+        move_piece(GB,black_pieces,red_pieces,piece,target_cords)
+        println("piece is",piece)
+      end
 
-    #   if(i%2==0)
-     piece = red_pieces.activeS[(source_cords)]
-    #  else
-    #    piece = black_pieces.activeS[(target_cords)]
-    #  end
-
-     move_piece(GB,red_pieces,black_pieces,piece,target_cords)
      display_board(GB,red_pieces,black_pieces)
      i = i+1
+     #println("i ",i)
      #the time
    end
 
 end
-run_from_Start(2,red_pieces,black_pieces)
+run_from_Start(7,red_pieces,black_pieces)
